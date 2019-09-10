a70ffbe8ecbe6d920b55a540febfb4cf61719027
Ankush Jain <akshay.jain521@gmail.com>
2019-09-10 16:12:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
- if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js')
-      .then(function(reg){
-        console.log("Yes, it did.");
-     }).catch(function(err) {
-        console.log("No it didn't. This happened:", err)
-    });
- }
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showInstallPromotion();
+});
